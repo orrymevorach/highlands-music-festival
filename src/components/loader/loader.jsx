98d99c02cfaf5badbfcac1ea8a9b10eff8bffc91
Orry Mevorach <orry.mevorach@gmail.com>
2022-07-14 00:52:45 -0400

    add loader to tickets page

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './loader.scss';
+
+export default function Loader() {
+  return (
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
--- src/components/tickets/tickets.jsx
+++ src/components/tickets/tickets.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoadEvenbriteOnPageLoad } from '@hooks';
 import './tickets.scss';
+import Loader from '@components/loader';
 
 export default function BuyTickets() {
+  const [showLoader, setShowLoader] = useState(true);
+  setTimeout(() => {
+    setShowLoader(false);
+  }, 500);
+
   useLoadEvenbriteOnPageLoad();
   return (
     <div className="widgetWrapper">
-      <div id="eventbrite-widget-container-353399967817"></div>
+      {showLoader && <Loader />}
+      <div
+        id="eventbrite-widget-container-353399967817"
+        className="widget"
+      ></div>
     </div>
   );
 }
