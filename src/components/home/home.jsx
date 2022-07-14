e4a22a421f49da4719063d070f4c10b321249eef
Orry Mevorach <orry.mevorach@gmail.com>
2022-07-14 02:02:07 -0400

    home page mobile

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,8 +4,10 @@ import { imgPath } from '../../utils/constants';
 import Icons from '../icons';
 import Layout from '../layout';
 import { Link } from 'gatsby';
+import { useWindowSize } from '@hooks';
 
 const Home = () => {
+  const { isMobile } = useWindowSize();
   return (
     <Layout hasPaddingBottom={false}>
       <h1 className="h1">Highlands Music Festival</h1>
@@ -15,7 +17,7 @@ const Home = () => {
           alt="Highlands Music Festival logo"
           className="logo"
         />
-        <Icons />
+
         <div className="videoContainer">
           <h2 className="date">Septemeber 30 -- October 2</h2>
           <video
@@ -25,11 +27,14 @@ const Home = () => {
             className="video"
             loop
           ></video>
-          <p className="ticketsAvailable">2022 tickets are now available</p>
-          <button className="buyNowButton">
-            <Link to="buy-tickets">Buy Now</Link>
-          </button>
+          <Link to="buy-tickets" className="buyNowButton">
+            Buy Now
+          </Link>
+          {!isMobile && (
+            <p className="ticketsAvailable">2022 tickets are now available</p>
+          )}
         </div>
+        <Icons />
       </div>
     </Layout>
   );
