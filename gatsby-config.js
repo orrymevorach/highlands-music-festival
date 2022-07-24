ee6f6edf9b5626f5ddcdf1bfa6506b6f0567670b
Orry Mevorach <orry.mevorach@gmail.com>
2022-07-24 16:08:38 -0400

    analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,19 @@ module.exports = {
         extensions: ['jsx', 'scss'],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-BDYFVYJ8SP', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ],
 };
