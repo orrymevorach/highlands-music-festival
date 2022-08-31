3daa99ad8db65afbc713195dd15b4b1f77dd4383
Orry Mevorach <orry.mevorach@gmail.com>
2022-08-31 16:27:19 -0400

    fix green sun sizing on lineup page

--- src/images/green-sun/green-sun.jsx
+++ src/images/green-sun/green-sun.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import './green-sun.scss';
-import clsx from 'clsx';
 
 export default function GreenSun({ classNames }) {
   return (
@@ -9,7 +7,7 @@ export default function GreenSun({ classNames }) {
       src="https://highlands-music-festival.s3.ca-central-1.amazonaws.com/Iconography/Iconography-03.png"
       alt=""
       placeholder="blurred"
-      className={clsx('sun', classNames)}
+      className={classNames}
     />
   );
 }
