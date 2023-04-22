f3531f7f1775f51d29da86c675010f9ed4315944
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-04-22 13:56:46 -0700

    google anaylytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import PageNotFound from './404';
 import { NavProvider } from 'context/nav-context';
+import Script from 'next/script';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
@@ -10,8 +11,27 @@ export default function App({ Component, pageProps }) {
   if (!isPagePublished) return <PageNotFound />;
 
   return (
-    <NavProvider navData={navData}>
-      <Component {...pageProps} />
-    </NavProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}, {
+          page_path: window.location.pathname,
+          });`,
+        }}
+      />
+
+      <NavProvider navData={navData}>
+        <Component {...pageProps} />
+      </NavProvider>
+    </>
   );
 }
