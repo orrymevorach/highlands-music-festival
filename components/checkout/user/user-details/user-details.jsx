495bdd91f3844875e577be179cb58050897894ba
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-01-16 07:41:47 -0500

    user review business name on checkout

--- components/checkout/user/user-details/user-details.jsx
+++ components/checkout/user/user-details/user-details.jsx
@@ -2,18 +2,33 @@ import { useCheckoutContext } from 'context/checkout-context';
 import styles from './user-details.module.scss';
 
 export default function UserDetails() {
-  const { customer } = useCheckoutContext();
+  const { customer, vendorName, vendorSecondGuest } = useCheckoutContext();
   return (
     <div className={styles.userDetails}>
-      <p className={styles.contactInformation}>Guest Information:</p>
-      <p className={styles.name}>
+      <p className={styles.bold}>Guest Information:</p>
+      <p>
         <span className={styles.left}>Name:</span>
-        <span className={styles.right}>{customer.name}</span>
+        <span>{customer.name}</span>
       </p>
-      <p className={styles.email}>
+      <p>
         <span className={styles.left}>Email:</span>
-        <span className={styles.right}>{customer.email}</span>
+        <span>{customer.email}</span>
       </p>
+      {vendorName && (
+        <div className={styles.vendorContainer}>
+          <p className={styles.bold}>Vendor Information:</p>
+          <p>
+            <span className={styles.left}>Business Name:</span>
+            <span>{vendorName}</span>
+          </p>
+          {vendorSecondGuest && (
+            <p>
+              <span className={styles.left}>Second Guest:</span>
+              <span>{vendorSecondGuest}</span>
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
