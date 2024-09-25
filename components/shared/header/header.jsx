c09824ac728865b17ced97796305f72309ad818b
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-09-25 17:30:13 -0400

    fix link

--- components/shared/header/header.jsx
+++ components/shared/header/header.jsx
@@ -3,8 +3,9 @@ import clsx from 'clsx';
 import Nav from 'components/nav';
 import Link from 'next/link';
 import { useWindowSize } from 'hooks';
-import logo from 'public/Logo-1200px-No-Bkgd-min-small.png'
+import logo from 'public/Logo-1200px-No-Bkgd-min-small.png';
 import Image from 'next/image';
+import { PAGE_SLUGS } from 'utils/constants';
 
 export default function Header({ hideBottomMargin, festivalDate = '' }) {
   const { isMobile } = useWindowSize();
@@ -21,10 +22,7 @@ export default function Header({ hideBottomMargin, festivalDate = '' }) {
       </div>
       <div className={clsx(styles.logoContainer, styles.grid)}>
         <Link href="/">
-          <Image
-            src={logo}
-            className={styles.thankYouLogo}
-          />
+          <Image src={logo} className={styles.thankYouLogo} />
         </Link>
         <h3>{festivalDate}</h3>
       </div>
@@ -37,7 +35,7 @@ export default function Header({ hideBottomMargin, festivalDate = '' }) {
             Lineup
           </Link>
           <Link
-            href="/buy-tickets"
+            href={PAGE_SLUGS.CHECKOUT}
             className={clsx(styles.buyTicketsButton, styles.grid)}
           >
             Buy Tickets
