e77103fe790e8fd66a402e14c14d60e6a6d8db3f
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-12-12 17:59:20 -0500

    fix pagination

--- components/PhotosPage/GalleryFlickr/pagination/pagination.jsx
+++ components/PhotosPage/GalleryFlickr/pagination/pagination.jsx
@@ -5,6 +5,7 @@ import {
   faChevronLeft,
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons';
+import { useWindowSize } from 'hooks';
 
 function calculateNumberOfPages(totalItems) {
   const itemsPerPage = 100;
@@ -13,36 +14,53 @@ function calculateNumberOfPages(totalItems) {
 }
 
 export default function Pagination({ setPage, page, numberOfItems }) {
+  const { isMobile } = useWindowSize();
   const numberOfPages = calculateNumberOfPages(numberOfItems);
-  const numberAsArray = Array.from(
-    { length: numberOfPages },
-    (_, index) => index
-  );
+  const visiblePages = 6;
+
+  const getVisibleNumbers = () => {
+    const start = Math.max(0, page - Math.ceil(visiblePages / 2));
+    const end = Math.min(numberOfPages, start + visiblePages);
+    const offsetStart = Math.max(0, end - visiblePages);
+    return Array.from(
+      { length: Math.min(visiblePages, numberOfPages) },
+      (_, index) => offsetStart + index
+    );
+  };
+  const numberAsArray = isMobile
+    ? getVisibleNumbers()
+    : Array.from({ length: numberOfPages }, (_, index) => index);
 
   const handleSetPage = page => {
-    window.scrollTo(0, 0);
-    setPage(page);
+    if (page > 0 && page <= numberOfPages) {
+      window.scrollTo(0, 0);
+      setPage(page);
+    }
   };
+
   return (
     <div className={styles.pagination}>
-      <button onClick={() => handleSetPage(page - 1)} className={styles.button}>
+      <button
+        onClick={() => handleSetPage(page - 1)}
+        className={styles.button}
+        disabled={page === 1}
+      >
         <FontAwesomeIcon icon={faChevronLeft} size="sm" />
       </button>
-      {numberAsArray.map(number => {
-        return (
-          <button
-            key={`pagination-${number}`}
-            onClick={() => handleSetPage(number + 1)}
-            className={clsx(
-              styles.button,
-              page === number + 1 && styles.active
-            )}
-          >
-            {number + 1}
-          </button>
-        );
-      })}
-      <button onClick={() => handleSetPage(page + 1)} className={styles.button}>
+      {numberAsArray.map(number => (
+        <button
+          key={`pagination-${number}`}
+          onClick={() => handleSetPage(number + 1)}
+          className={clsx(styles.button, page === number + 1 && styles.active)}
+        >
+          {number + 1}
+        </button>
+      ))}
+      <button
+        onClick={() => handleSetPage(page + 1)}
+        className={styles.button}
+        disabled={page === numberOfPages}
+      >
         <FontAwesomeIcon icon={faChevronRight} size="sm" />
       </button>
     </div>
