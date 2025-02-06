ce2972bc25c061ed7ad8d197770512ff70ff448f
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-02-06 00:52:00 -0500

    close artist submissions

--- components/ArtistSubmissionsPage/ArtistSubmissions.jsx
+++ components/ArtistSubmissionsPage/ArtistSubmissions.jsx
@@ -2,6 +2,8 @@ import styles from './ArtistSubmissions.module.scss';
 import ArtistSubmissionForm from './ArtistSubmissionForm/ArtistSubmissionForm';
 
 export default function ArtistSubmission() {
+  const year = new Date().getFullYear();
+  const nextYear = year + 1;
   return (
     <main>
       <div className={styles.textContainer}>
@@ -10,26 +12,26 @@ export default function ArtistSubmission() {
           Welcome to the Highlands Music Festival Artist Submission form!
         </p>
         <p className={styles.text}>
-          We accept submissions all year round, however submissions must be
+          {/* We accept submissions all year round, however submissions must be
           received by January 31 in order to be considered for the following
-          year’s Festival.
-          {/* We accept submissions all year round, however submissions for 2024 are
-          now closed. Stay tuned for more info about HMF 2025 submission
-          deadlines. */}
+          year’s Festival. */}
+          We accept submissions all year round, however submissions for {year}{' '}
+          are now closed. Stay tuned for more info about HMF {nextYear}{' '}
+          submission deadlines.
         </p>
-        <p className={styles.text}>
-          {/* HMF 2024 will take place on Sept 25-28. We will notify you with any
+        {/* <p className={styles.text}> */}
+        {/* HMF 2024 will take place on Sept 25-28. We will notify you with any
           further questions or confirmation. Please be patient as we receive a
           high volume of submissions. */}
-          We will notify you with any further questions or confirmation. Please
-          be patient as we receive a high volume of submissions.
-        </p>
-        <p className={styles.text}>
-          Please note: HMF takes place at Camp Walden approximately 3 hours away
-          from Toronto and 2 from Ottawa, and we do not provide transportation.
-        </p>
+        {/* We will notify you with any further questions or confirmation. Please
+          be patient as we receive a high volume of submissions. */}
+        {/* </p> */}
+        {/* <p className={styles.text}> */}
+        {/* Please note: HMF takes place at Camp Walden approximately 3 hours away */}
+        {/* from Toronto and 2 from Ottawa, and we do not provide transportation. */}
+        {/* </p> */}
 
-        <ArtistSubmissionForm />
+        {/* <ArtistSubmissionForm /> */}
       </div>
     </main>
   );
