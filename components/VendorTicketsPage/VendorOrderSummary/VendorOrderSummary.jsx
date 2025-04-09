2926d823c748b189171c66d3221be0420a4d5d43
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-04-08 23:30:10 -0700

    touch ups

--- components/VendorTicketsPage/VendorOrderSummary/VendorOrderSummary.jsx
+++ components/VendorTicketsPage/VendorOrderSummary/VendorOrderSummary.jsx
@@ -4,23 +4,27 @@ import OrderSummaryLayout, {
 } from 'components/shared/OrderSummaryLayout/OrderSummaryLayout';
 import Loader from 'components/shared/Loader/Loader';
 import { calculateCart } from 'components/CheckoutPage/checkout-utils';
+import Button from 'components/shared/Button/Button';
 
 export default function OrderSummary({ isLoading, cart }) {
   const { subtotal, tax, total } = calculateCart({ items: cart });
   return (
-    <OrderSummaryLayout
-      subtotal={subtotal}
-      tax={tax}
-      total={total}
-      classNames={styles.container}
-    >
-      {isLoading ? (
-        <Loader centerInContainer />
-      ) : (
-        cart.map(item => {
-          return <LineItem label={item.name} price={item.price} />;
-        })
-      )}
-    </OrderSummaryLayout>
+    <div className={styles.outerContainer}>
+      <OrderSummaryLayout
+        subtotal={subtotal}
+        tax={tax}
+        total={total}
+        classNames={styles.container}
+      >
+        {isLoading ? (
+          <Loader centerInContainer />
+        ) : (
+          cart.map(item => {
+            return <LineItem label={item.name} price={item.price} />;
+          })
+        )}
+      </OrderSummaryLayout>
+      <Button classNames={styles.button}>Checkout</Button>
+    </div>
   );
 }
